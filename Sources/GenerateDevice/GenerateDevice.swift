f0bf759d275ad48798665949950e431a00605938
YujinGaya <yujin.gaya@gmail.com>
2022-12-25 16:02:52 +0900

    Add --output option to write to a file

--- Sources/GenerateDevice/GenerateDevice.swift
+++ Sources/GenerateDevice/GenerateDevice.swift
@@ -7,6 +7,17 @@ import StencilSwiftKit
 
 @main
 struct GenerateDevice: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Generate Device.swift from a device_traits.db file",
+        usage: """
+            generate-device [--output <output>] <device-traits>
+            """,
+        version: "0.1.0"
+    )
+
+    @Option(name: [.short, .customLong("output")], help: "Write output to a specified file")
+    var outputFile: String?
+
     @Argument(help: "The path to device_traits.db file", completion: .file(extensions: ["db"]))
     var deviceTraits: String
 
@@ -20,6 +31,11 @@ struct GenerateDevice: ParsableCommand {
 
         let rendered = try environment.renderTemplate(name: "Templates/Device.swift.stencil", devices: devices)
 
-        print(rendered)
+        if let outputFile {
+            try rendered.write(toFile: outputFile, atomically: true, encoding: .utf8)
+        }
+        else {
+            print(rendered)
+        }
     }
 }
